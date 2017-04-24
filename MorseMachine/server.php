4d84b041498ec8f8143bf376d09ac77f9b7064a0
Matt <mattcarus@gmail.com>
2017-04-24 14:29:04 +0100

    Added server.php

---
+++ MorseMachine/server.php
@@ -0,0 +1,39 @@
+<?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+// Make sure composer dependencies have been installed
+require __DIR__ . '/vendor/autoload.php';
+
+class Chat implements MessageComponentInterface {
+	protected $clients;
+
+	public function __construct() {
+		$this->clients = new \SplObjectStorage;
+	}
+
+	public function onOpen(ConnectionInterface $conn) {
+		$this->clients->attach($conn);
+	}
+
+	public function onMessage(ConnectionInterface $from, $msg) {
+		foreach ($this->clients as $client) {
+			if ($from != $client) {
+				$client->send($msg);
+			}
+		}
+	}
+
+	public function onClose(ConnectionInterface $conn) {
+		$this->clients->detach($conn);
+	}
+
+	public function onError(ConnectionInterface $conn, \Exception $e) {
+		$conn->close();
+	}
+}
+
+$app = new Ratchet\App('localhost', 8080);
+$app->route('/chat', new Chat);
+$app->route('/echo', new Ratchet\Server\EchoServer, array('*'));
+$app->run();
